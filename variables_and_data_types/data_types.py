ad6595c57fd270ad974d1282ff892934422be1e6
Muhammad Usman <mu17216@gmail.com>
2026-01-26 15:04:26 +0500

    data types

---
+++ variables_and_data_types/data_types.py
@@ -0,0 +1,14 @@
+# string in text 
+name = 'Muhammad Usman'
+print(name,type(name))
+
+# integer is whole number 
+age = 26
+print(age,type(age))
+
+# float A number with a decimal
+price = 99.9
+
+# Logical values that are either True or False.
+is_student = True
+print(is_student,)
